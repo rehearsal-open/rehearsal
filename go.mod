71981b2fadeb7e49693d15f59b5b05bcdf86fd6b
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-06-21 11:25:51 +0900

    yamlとjsonを読み込む関数を実装

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/rehearsal-open/rehearsal
 
 go 1.16
 
-require gopkg.in/yaml.v2 v2.4.0 // indirect
+require (
+	github.com/pkg/errors v0.9.1
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+)
---
+++ util/util.go
@@ -0,0 +1,42 @@
+package algorithm
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+
+	"gopkg.in/yaml.v3"
+
+	"github.com/pkg/errors"
+)
+
+func FileExist(filename string) bool {
+	_, err := os.Stat(filename)
+	return !os.IsNotExist(err)
+}
+
+func MarkUpFileLoad(filename string) (string, map[string]interface{}, error) {
+	buf, err := ioutil.ReadFile(filename)
+
+	if err != nil {
+		return "", nil, errors.WithStack(err)
+	}
+
+	var result map[string]interface{}
+	ext := filepath.Ext(filename)
+
+	switch ext {
+	case ".yaml":
+	case ".yml":
+		err = yaml.Unmarshal(buf, &result)
+		break
+	case ".json":
+		err = json.Unmarshal(buf, &result)
+		break
+	default:
+		return ext, nil, errors.New("Invalid File Extension: " + ext)
+	}
+
+	return ext, result, nil
+}
