bc8a7e3823df0004ee45dee06d7e8534b2af8adf
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-06-22 15:05:05 +0900

    algorithm/queue.goのコメントアウト

--- algorithm/queue.go
+++ algorithm/queue.go
@@ -4,6 +4,7 @@ import (
 	"github.com/pkg/errors"
 )
 
+// Queue algorithm object
 type SimpleQueue struct {
 	data    []interface{}
 	headIdx uint64
@@ -11,6 +12,7 @@ type SimpleQueue struct {
 	length  uint64
 }
 
+// Constractor of algorithm.SingleQueue.
 func SimpleQueueMaker(margin uint64) SimpleQueue {
 
 	queue := SimpleQueue{
@@ -23,10 +25,12 @@ func SimpleQueueMaker(margin uint64) SimpleQueue {
 	return queue
 }
 
+// Return length of queue's internal array (internal method).
 func (queue *SimpleQueue) ability() uint64 {
 	return uint64(len(queue.data))
 }
 
+// Return result of increment index (internal method).
 func (queue *SimpleQueue) increment(idx uint64) uint64 {
 	idx++
 	if idx == queue.ability() {
@@ -35,6 +39,7 @@ func (queue *SimpleQueue) increment(idx uint64) uint64 {
 	return idx
 }
 
+// Return result of addition index (internal method).
 func (queue *SimpleQueue) add(idx uint64, addVal uint64) uint64 {
 	ability := queue.ability()
 	remain := ability - idx
@@ -46,6 +51,7 @@ func (queue *SimpleQueue) add(idx uint64, addVal uint64) uint64 {
 	return idx
 }
 
+// Push back the object into queue.
 func (queue *SimpleQueue) Append(object interface{}) {
 
 	ability := queue.ability()
@@ -71,6 +77,7 @@ func (queue *SimpleQueue) Append(object interface{}) {
 	}
 }
 
+// Return the front value; this func doesn't pop (i.e: don't remove front value from queue).
 func (queue *SimpleQueue) Get() (interface{}, error) {
 	if queue.length < 1 {
 		return nil, errors.New("Queue is empty")
@@ -78,6 +85,17 @@ func (queue *SimpleQueue) Get() (interface{}, error) {
 	return queue.data[queue.headIdx], nil
 }
 
+// Return the front value and remove front value from queue.
+func (queue *SimpleQueue) GetPop() (result interface{}, err error) {
+	result, err = queue.Get()
+	if err != nil {
+		return nil, errors.WithStack(err)
+	}
+	queue.Pop()
+	return result, err
+}
+
+// Remove front value from queue.
 func (queue *SimpleQueue) Pop() {
 	if queue.length > 0 {
 		queue.length--
@@ -85,6 +103,7 @@ func (queue *SimpleQueue) Pop() {
 	}
 }
 
+// Return length of queue, it is on the surface.
 func (queue *SimpleQueue) Length() uint64 {
 	return queue.length
 }
