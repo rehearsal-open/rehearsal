7d8357b6ff44a544dab2f83dd8a11c1e9940c6c5
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-06-30 12:47:55 +0900

    100個現れるテストケースのうち一つに絞る

---
+++ test/ahc003/communicator.py
@@ -0,0 +1,31 @@
+horizonal = []
+vertical = []
+
+sPos = [list(), list()]
+ePos = [list(), list()]
+minLength = list()
+rate = list()
+
+i = 1
+
+for _ in range(1059):
+    print(input())
+
+# for _ in range(100):
+#     horizonal = horizonal + [list(map(int, input().split())) for _ in range(30)]
+#     vertical = vertical + [list(map(int, input().split())) for _ in range(29)]
+
+#     for _ in range(1000):
+#         s1, s2, e1, e2, ml, r = map(float, input().split())
+#         sPos[0].append(int(s1))
+#         sPos[1].append(int(s2))
+#         ePos[0].append(int(e1))
+#         ePos[1].append(int(e2))
+#         minLength.append(int(ml))
+#         rate.append(r)
+
+#     input()
+#     print(len(horizonal))
+#     i += 1
+
+    
