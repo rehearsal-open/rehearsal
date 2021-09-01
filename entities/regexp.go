ddc7b724b3ee602b735e646bbc2297fad158b3d4
Kawanishi Hirotsugu <80010105+StreamWest-1629@users.noreply.github.com>
2021-09-01 15:53:46 +0900

    Revert "エンティティを追加"
    This reverts commit cfe3aa6f2e9b96bba14ccce2e8cea5e8b0d053c0.

--- entities/regexp.go
+++ entities/regexp.go
@@ -17,7 +17,9 @@
 package entities
 
 import (
-	"github.com/streamwest-1629/textfilter"
+	"regexp"
+
+	"github.com/streamwest-1629/guarantee/filter"
 )
 
 const (
@@ -27,13 +29,21 @@ const (
 	systemDefinedNameExpr = "__[a-zA-Z][a-zA-Z0-9_-]*"
 	// for user and system defined name (tasks, phases, and so on...)
 	definedNameExpr = "(__)?[a-zA-Z][a-zA-Z0-9_-]*"
+	// for task's fullname, including phase and task's name
+	taskFullNameExpr = definedNameExpr + "::" + definedNameExpr
+	// for user defined task's fullname, including phase and task user's defined name
+	userTaskFullNameExpr = userDefinedNameExpr + "::" + userDefinedNameExpr
 )
 
 var (
 	// Filter for user defined name (tasks, phases, and so on...).
-	IsUserDefinedName = textfilter.RegexpExactMatches(userDefinedNameExpr)
+	UserDefinedName = filter.RegexpFilter(regexp.MustCompile(userDefinedNameExpr))
 	// Filter for system defined name (tasks, phases, and so on...).
-	IsSystemDefiedName = textfilter.RegexpExactMatches(systemDefinedNameExpr)
+	SystemDefiedName = filter.RegexpFilter(regexp.MustCompile(systemDefinedNameExpr))
 	// Filter for user and system defined name (tasks, phases, and so on...).
-	IsDefinedName = textfilter.RegexpExactMatches(definedNameExpr)
+	DefinedName = filter.RegexpFilter(regexp.MustCompile(definedNameExpr))
+	// Filter for task's fullname, including phase and task's name.
+	TaskFullName = filter.RegexpFilter(regexp.MustCompile(taskFullNameExpr))
+	// Filter for user defined task's fullname, including phase and task user's defined name.
+	UserTaskFullName = filter.RegexpFilter(regexp.MustCompile(userTaskFullNameExpr))
 )
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/rehearsal-open/rehearsal
 
 go 1.16
 
-require github.com/streamwest-1629/textfilter v0.0.0-20210815030443-cbe89bd4db9c
+require (
+	github.com/streamwest-1629/guarantee v0.0.0-20210814050127-9ed89127188e // indirect
+	github.com/streamwest-1629/guarantee_str v0.0.0-20210813070503-2636351578a9
+)
