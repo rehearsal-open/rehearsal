678c433b59f87214a856bf53be0b283ee1f731d0
Kasai Koji <80010105+StreamWest-1629@users.noreply.github.com>
2021-09-02 10:58:16 +0900

    テスト内容を変更

--- test/py2py/01/python1.py
+++ test/py2py/01/python1.py
@@ -1,10 +1,5 @@
 from time import sleep
 
-w = open("in.txt", 'w')
-
 for i in range(10000):
     a = int(input())
     print(a * a, flush=True)
-    print(a * a, file=w)
-    
-    # sleep(1)
--- test/py2py/01/python3.py
+++ test/py2py/01/python3.py
@@ -1,6 +1,6 @@
 from time import sleep
 
-for i in range(10000000):
+for i in range(1000000):
     a = int(input())
     print(a * a, flush=True)
     
