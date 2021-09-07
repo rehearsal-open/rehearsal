9d38a8e392b2795412b2e7d3247530ab567ee5ec
Kasai Koji <80010105+StreamWest-1629@users.noreply.github.com>
2021-09-07 10:53:38 +0900

    リスナーを追加

---
+++ task/wrapper/listen/routine.go
@@ -0,0 +1,65 @@
+// task/listen/routine.go
+// Copyright (C) 2021 Kasai Koji
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package listen
+
+import (
+	"bytes"
+	"io"
+
+	"github.com/rehearsal-open/rehearsal/entities"
+	"github.com/rehearsal-open/rehearsal/entities/enum/task_element"
+	"github.com/rehearsal-open/rehearsal/task"
+	"github.com/rehearsal-open/rehearsal/task/wrapper"
+)
+
+var (
+	OnFinalDefault = func() {}
+	OnErrDefault   = func(error) {}
+)
+
+func Listen(fromTask task.Task, fromElem task_element.Enum, destInput io.Writer, onErr func(error), onFinal func()) {
+
+	// set default value
+	if onErr == nil {
+		onErr = OnErrDefault
+	}
+	if onFinal == nil {
+		onFinal = OnFinalDefault
+	}
+
+	// get queue access
+	from := wrapper.GetQueueAccess(fromTask)
+
+	// get element queue
+	reader := from.GetInput(fromElem)
+
+	go func() {
+		for isContinue := true; isContinue; {
+			reader.Read(func(e *entities.Element, b []byte) {
+				if e != nil {
+					if _, err := io.Copy(destInput, bytes.NewBuffer(b)); err != nil {
+						if err != io.EOF {
+							onErr(err)
+						}
+					}
+				} else {
+					onFinal()
+				}
+			})
+		}
+	}()
+}
