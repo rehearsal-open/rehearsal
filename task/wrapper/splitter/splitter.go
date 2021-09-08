71bd6ac1500ad59a945c5f83450d436e51a37c31
Kasai Koji <80010105+StreamWest-1629@users.noreply.github.com>
2021-09-08 12:19:35 +0900

    wrapper.splitterを追加

---
+++ task/wrapper/splitter/splitter.go
@@ -0,0 +1,55 @@
+// task/wrapper/splitter/splitter.go
+// Copyright (C) 2021 Kasai Koji
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+package splitter
+
+import (
+	"io"
+	"strings"
+
+	"github.com/rehearsal-open/rehearsal/entities"
+)
+
+func (splitter *Splitter) Write(e *entities.Element, b []byte) error {
+
+	splitter.cache += string(b)
+
+	splitted := []string{splitter.cache}
+
+	for _, splitStr := range splitter.SplitStr {
+		splittedCache := splitted
+		splitted = make([]string, 0)
+
+		for _, val := range splittedCache {
+			splitted = append(splitted, strings.Split(val, splitStr)...)
+		}
+	}
+
+	for i, l := 0, len(splitted)-1; i < l; i++ {
+		splitter.writer.Write([]byte(splitted[i]))
+	}
+
+	splitter.cache = splitted[len(splitted)-1]
+	return nil
+}
+
+func (splitter *Splitter) Close() {
+	splitter.writer.Write([]byte(splitter.cache))
+}
+
+func (splitter *Splitter) OutputTo(outto io.Writer) {
+	splitter.writer = outto
+}
